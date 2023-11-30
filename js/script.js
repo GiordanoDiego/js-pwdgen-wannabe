3a179e6ac45f7c9f69fbe8e0110d0ff7a6e043bc
GiordanoDiego <diegomgiordano96@gmail.com>
2023-11-30 15:21:51 +0100

    finished

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+let nomeUtente = prompt('Inserisci nome:');
+console.log('Valore preso dal prompt:', nomeUtente);
+document.getElementById('nomeUtente').innerHTML = nomeUtente;
+
+let cognomeUtente = prompt('Inserisci cognome:');
+console.log('Valore preso dal prompt:', cognomeUtente);
+document.getElementById('cognomeUtente').innerHTML = cognomeUtente;
+
+let coloreUtente = prompt('Inserisci il tuo colore preferito:');
+console.log('Valore preso dal prompt:', coloreUtente);
+document.getElementById('coloreUtente').innerHTML = coloreUtente;
+
+let datiUtente = nomeUtente + " " + cognomeUtente + " " + coloreUtente + " " + 114;
+console.log('dati complreti:', datiUtente)
+document.getElementById('datiUtente').innerHTML = datiUtente;
+ 
